892b22bb59bf3d7e99544f10c40da0d2f69eae22
Abu Omayer <abuomayer.111@gmail.com>
2025-03-10 23:04:11 +0600

    route protection doen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,17 +8,26 @@ import SettingsPage from './pages/SettingsPage'
 import ProfilePage from './pages/ProfilePage'
 import useAuthStore from './store/useAuthStore.js'
 import { useEffect } from 'react'
-
-
+import {Loader} from 'lucide-react'
+import {Navigate} from 'react-router-dom'
 
 const App = () => {
   
-  const {authUser,checkAuth} = useAuthStore();
+  const {authUser,checkAuth,isCheckingAuth} = useAuthStore();
   
   useEffect(() => {
     checkAuth();
   }
   , [checkAuth]);
+
+  if(!authUser && isCheckingAuth === true){
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Loader className='size-10 animate-spin' />
+      </div>
+    )
+  }
+
 
   console.log(authUser);
 
@@ -28,11 +37,11 @@ const App = () => {
       <Navbar />
       <Routes>
       
-        <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={authUser? <HomePage /> : <Navigate to="/login" /> } />
+        <Route path="/register" element={authUser? <Navigate to='/' /> : <RegisterPage /> } />
+        <Route path="/login" element={authUser? <Navigate to='/' /> : <Login /> } />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile" element={authUser? <ProfilePage /> : <Navigate to="/login" /> } />
       
       </Routes>
     </div>
